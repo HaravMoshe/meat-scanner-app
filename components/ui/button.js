900c9600e041fe4b0f53539bb26118879cd20736
HaravMoshe <moshe@vlandata.com>
2025-05-07 17:40:23 -0400

    V1.2

---
+++ components/ui/button.js
@@ -0,0 +1,11 @@
+export function Button({ children, onClick, variant = 'default', ...props }) {
+  return (
+    <button
+      onClick={onClick}
+      className={\`\${variant === 'outline' ? 'border border-gray-400' : 'bg-blue-600 text-white'} px-4 py-2 rounded\`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
